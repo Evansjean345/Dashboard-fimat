7da658956a642a0681357909a1b040d34e6ff986
Evansjean345 <Evansjean345@gmail.com>
2023-05-19 19:43:17 +0000

    true

--- client/src/services/account.service.js
+++ client/src/services/account.service.js
@@ -5,15 +5,15 @@ import { Navigate, useNavigate } from "react-router-dom";
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const [userId, setUserId] = useState(localStorage.getItem("userId") || null);
-  const [token, setToken] = useState(localStorage.getItem("token") || null);
+  const [userId, setUserId] = useState(Cookies.get("userId") || null);
+  const [token, setToken] = useState(Cookies.get("token") || null);
   const navigate = useNavigate();
 
   const login = ({ userId, token }) => {
     setUserId(userId);
     setToken(token);
-    localStorage.setItem("userId", userId);
-    localStorage.setItem("token", token);
+    Cookies.set("userId", userId);
+    Cookies.set("token", token);
     Cookies.get("jwt");
     console.log(userId, token);
   };
@@ -21,28 +21,25 @@ const AuthProvider = ({ children }) => {
   const logout = () => {
     setUserId(null);
     setToken(null);
-    localStorage.removeItem("userId");
-    localStorage.removeItem("token");
+    Cookies.remove("userId");
+    Cookies.remove("token");
     navigate("/");
   };
 
   const isAuthenticated = () => {
-    const _id = userId;
-    return token !== null && userId === _id ? token : undefined;
+    const _id = userId
+    return token !== null && userId === _id ? token  : undefined;
   };
 
+
   const getUserInfo = async () => {
-    const response = await fetch(
-      `https://fimat-group-api.onrender.com/user/${userId}`
-    );
+    const response = await fetch(`https://fimat-group-api.onrender.com/user/${userId}`);
     const data = await response.json();
     return data;
   };
 
   const getOrderByUser = async () => {
-    const response = await fetch(
-      `https://fimat-group-api.onrender.com/user/${userId}/order`
-    );
+    const response = await fetch(`https://fimat-group-api.onrender.com/user/${userId}/order`);
     const data = await response.json();
     return data;
   };
